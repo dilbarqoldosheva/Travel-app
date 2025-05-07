a0d27d14c76fb29fff815b8e312224850cba9a26
dilbarqoldosheva <bintushokir07@gmail.com>
2025-05-08 00:28:54 +0500

    add new section

--- components/Home/WhyChoose/WhyCHooseCard.tsx
+++ components/Home/WhyChoose/WhyCHooseCard.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
-function WhyCHooseCard() {
+type Props = {
+  image: string
+  title: string
+}
+const WhyCHooseCard = ({ image, title }: Props) => {
   return (
-    <div>WhyCHooseCard</div>
+    <div>
+     <Image src={image} width={70} height={70} alt='image' className='mx-auto'/>
+     <h1 className='mt-6 text-center font-medium text-gray-700 text-lg'>{title}</h1>
+     <p className='mt-2 text-center text-xs font-medium text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet.</p>
+    </div>
   )
 }
 
--- components/Home/WhyChoose/WhyChoose.tsx
+++ components/Home/WhyChoose/WhyChoose.tsx
@@ -1,11 +1,22 @@
 import SectionHeading from '@/components/Helper/SectionHeading'
 import React from 'react'
+import WhyCHooseCard from './WhyCHooseCard'
 
 function WhyChoose() {
   return (
-    <div className='pt-16-pb-24'>
-        <SectionHeading heading='Why Choose Us'/>
-        <div className='grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20 '></div>
+    <div className='pt-16 pb-24'>
+      <SectionHeading heading='Why Choose Us' />
+      <div className='grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20 '>
+        <div>
+          <WhyCHooseCard image="/image/c1.svg" title="Best Price Guarantee" />
+        </div>
+        <div>
+          <WhyCHooseCard image="/image/c2.svg" title="Easy & Quick Booking" />
+        </div>
+        <div>
+          <WhyCHooseCard image="/image/c3.svg" title="Customer Care 24/7" />
+        </div>
+      </div>
     </div>
   )
 }
