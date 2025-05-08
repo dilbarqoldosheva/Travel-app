4e34c0ff0d3cac5f545ea48ac18b2c31ab360f7c
dilbarqoldosheva <bintushokir07@gmail.com>
2025-05-08 11:39:59 +0500

    finesh

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ResponsiveNav/>
         {children}
         <Footer/>
+        <ScrollToTop/>
       </body>
     </html>
   );
